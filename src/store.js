87cf1ce7aea2f1a93cdce23c631a35c44de1c315
Bec Scott <me@becdetat.com>
2019-12-30 14:42:06 +1000

    Persist pages to local storage

--- src/store.js
+++ src/store.js
@@ -1,4 +1,21 @@
 import { createStore } from 'redux';
 import rootReducer from './reducers';
+import {
+  loadStateFromLocalStorage,
+  saveStateToLocalStorage
+} from './utilities/localStorageHelper';
+import throttle from 'lodash/throttle'
 
-export default createStore( rootReducer );
+const state = loadStateFromLocalStorage();
+const store = createStore( rootReducer, state );
+
+const saveState = () => {
+  console.log(store.getState())
+  saveStateToLocalStorage( store.getState() );
+  // This is where remote sync would be started
+};
+const throttledSaveState = throttle( saveState, 100 );
+
+store.subscribe( throttledSaveState );
+
+export default store;
---
+++ src/utilities/localStorageHelper.js
@@ -0,0 +1,21 @@
+export function loadStateFromLocalStorage() {
+  try {
+    const serializedState = localStorage.getItem( 'state' );
+
+    if ( serializedState === null ) {
+      return {};
+    }
+
+    return JSON.parse( serializedState );
+  } catch ( error ) {
+    return {};
+  }
+}
+
+export function saveStateToLocalStorage( state ) {
+  try {
+    const serializedState = JSON.stringify( state );
+
+    localStorage.setItem( 'state', serializedState );
+  } catch {}
+}
